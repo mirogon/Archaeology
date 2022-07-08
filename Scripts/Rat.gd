af5718b9ca36c830b47292f8e08ac35497dfb1b3
Marcel Römer <m1smr@hotmail.com>
2022-07-08 18:29:45 +0200

    Removed old TestMap, Rat now changes direction

--- Scripts/Rat.gd
+++ Scripts/Rat.gd
@@ -3,16 +3,19 @@ extends KinematicBody2D
 export var speed = 75
 var player
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_root().get_node("Main").get_node("Player")
 
 func _physics_process(delta):
 	if position.distance_to(player.position) > 15:
-		#position = position.move_toward(player.position, speed * delta)
 		var dir = (player.position - position).normalized()
 		move_and_slide(dir * speed)
 		$AnimatedSprite.animation = "Rat_Walk"
+		
+		if position.x > player.position.x:
+			$AnimatedSprite.flip_h = false
+		else:
+			$AnimatedSprite.flip_h = true
 	else:
 		$AnimatedSprite.animation = "Rat_Atk"
 
