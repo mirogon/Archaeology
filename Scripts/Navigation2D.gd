816991dd81e1a797d4cc55b93cf2024edebb9059
Marcel Römer <m1smr@hotmail.com>
2022-07-08 17:16:09 +0200

    Navigation System movement

--- Scripts/Navigation2D.gd
+++ Scripts/Navigation2D.gd
@@ -1,18 +1,12 @@
 extends Navigation2D
-
-var destination: Vector2
-var npc
 var closestPath
 var currentPointIndex
 var destination_reached
+export var move_speed = 50
 
 func _ready():
-	var goalNode: Node2D = $Goal
-	destination = goalNode.position
-
-	npc = $Npc
 	
-	closestPath = get_simple_path(npc.position, destination)
+	closestPath = get_simple_path($Npc.position, $Goal.position)
 	closestPath.remove(0)
 	
 	print(closestPath.size())
@@ -21,13 +15,22 @@ func _ready():
 	currentPointIndex = 0
 	destination_reached = false
 
-func move_to_next_point():
-	npc.position = closestPath[currentPointIndex]
-	currentPointIndex += 1
-	if(currentPointIndex >= closestPath.size()):
-		destination_reached = true
+func get_current_destination():
+	var distance_to_point_vector: Vector2 = closestPath[currentPointIndex] - $Npc.position
+	var distance_to_point: float = abs(distance_to_point_vector.length())
+	if(distance_to_point < 10):
+		currentPointIndex += 1
+		if(currentPointIndex >= closestPath.size()):
+			destination_reached = true
+			currentPointIndex -= 1
+	return closestPath[currentPointIndex]
+
+func move_closer_to(destination: Vector2, speed_times_delta):
+	var direction: Vector2 = destination - $Npc.position
+	direction = direction.normalized()
+	$Npc.position += direction * speed_times_delta
 
 func _process(delta):
-	if(destination_reached == false):
-		if(Input.is_action_just_pressed("ui_down")):
-			move_to_next_point()
+	if(!destination_reached):
+		var next_destination = get_current_destination()
+		move_closer_to(next_destination, move_speed * delta)
