40aca2bb27f5a1d9c98bdd2866b966ee3ce565e9
Marcel Römer <m1smr@hotmail.com>
2022-07-10 12:06:11 +0200

    New Navigation Test with Soldier

--- Scenes/NavigationMovement.gd
+++ Scenes/NavigationMovement.gd
@@ -1,7 +1,70 @@
 extends Node
 
-func _ready():
-	pass # Replace with function body.
+export var moveSpeed = 100
+export var mouse_debug_mode = false
 
-#func _process(delta):
-#	pass
+#Expects a Navigation2D in the running root scene
+var navigation2D: Navigation2D
+
+var current_destination: Vector2
+var new_destination: bool = false
+var current_path: PoolVector2Array
+var current_path_index: int
+var destination_reached: bool
+
+var active = false
+
+func _ready():
+	navigation2D = get_tree().get_root().get_node("Navigation2D")
+	if(navigation2D):
+		active = true
+		
+	destination_reached = false
+	current_path_index = 0
+	set_new_destination(Vector2(0,270))
+
+func set_new_destination(dest: Vector2):
+	current_destination = dest
+	new_destination = true
+
+func get_new_path():
+	current_path = navigation2D.get_simple_path(get_parent().position, current_destination)
+	current_path_index = 0
+
+func increment_path_index():
+	current_path_index += 1
+	if(current_path_index >= current_path.size()):
+		destination_reached = true
+		current_path_index -= 1
+
+func get_current_path_point_destination():
+	var distance_vector = current_path[current_path_index] - get_parent().position
+	var distance_value = abs(distance_vector.length())
+	
+	if(distance_value < 10):
+		increment_path_index()
+		
+	return current_path[current_path_index]
+
+func move_closer_to(destination: Vector2, speed_times_delta: float):
+	var dir: Vector2 = destination - get_parent().position
+	dir = dir.normalized()
+	get_parent().position += dir * speed_times_delta
+
+func mouse_debug_mode_set_position():
+	if Input.is_mouse_button_pressed(1):
+		var parent = get_parent() as Node2D
+		set_new_destination(parent.get_global_mouse_position())
+		
+func _process(delta):
+	if(!active):
+		return
+		
+	if(mouse_debug_mode):
+		mouse_debug_mode_set_position()
+		
+	if(new_destination):
+		get_new_path()
+	var current_path_point_destination = get_current_path_point_destination()
+	move_closer_to(current_path_point_destination, moveSpeed * delta)
+	
