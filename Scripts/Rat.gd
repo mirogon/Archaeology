fb4aae8ef08cb0f3c03e827fa20f56dcd3ac5453
Marcel Römer <m1smr@hotmail.com>
2022-07-10 15:06:39 +0200

    Rat now attacks Soldier

--- Scripts/Rat.gd
+++ Scripts/Rat.gd
@@ -2,23 +2,25 @@ extends KinematicBody2D
 
 export var speed = 75
 var player
+var soldier
 
 func _ready():
 	player = get_tree().get_root().get_node("Main").get_node("Player")
+	soldier = get_tree().get_root().get_node("Main").get_node("Soldier")	
 
 func _physics_process(delta):
-	if position.distance_to(player.position) > 15:
-		var dir = (player.position - position).normalized()
+	if position.distance_to(soldier.position) > 15:
+		var dir = (soldier.position - position).normalized()
 		move_and_slide(dir * speed)
 		$AnimatedSprite.animation = "Rat_Walk"
 		
-		if position.x > player.position.x:
+		if position.x > soldier.position.x:
 			$AnimatedSprite.flip_h = false
 		else:
 			$AnimatedSprite.flip_h = true
 	else:
 		$AnimatedSprite.animation = "Rat_Atk"
-		var player_health_system = player.get_node("HealthSystem") as HealthSystem
+		var player_health_system = soldier.get_node("HealthSystem") as HealthSystem
 		player_health_system.take_damage(1)
 
 func _process(delta):
