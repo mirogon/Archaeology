12330a86e8900d1d771c9a08d35e0ac04f1813d7
Marcel Römer <m1smr@hotmail.com>
2022-07-11 15:27:59 +0200

    Changed damage values from Projectiles

--- Scripts/GunProjectile.gd
+++ Scripts/GunProjectile.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 class_name GunProjectile
 
-export var hitDamage = 100
+export var hitDamage = 50
 export var moveSpeed = 150
 
 var moveDir: Vector2
@@ -10,10 +10,8 @@ var moveDir: Vector2
 func _ready():
 	pass
 
-
 func _process(delta):
 	position += moveDir * delta * moveSpeed
-
 
 func _on_Projectile_body_entered(body):
 	if body.is_in_group("Enemy"):
--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 class_name Projectile
 
-export var hitDamage = 100
+export var hitDamage = 10
 export var moveSpeed = 150
 
 var moveDir: Vector2
@@ -10,10 +10,8 @@ var moveDir: Vector2
 func _ready():
 	$AnimatedSprite.frame = 0
 
-
 func _process(delta):
 	position += moveDir * delta * moveSpeed
-
 
 func _on_Projectile_body_entered(body):
 	if body.is_in_group("Enemy"):
@@ -22,10 +20,8 @@ func _on_Projectile_body_entered(body):
 		healthSystem.take_damage(hitDamage)
 		moveSpeed = 0
 
-
 func _on_AnimatedSprite_animation_finished():
 	queue_free()
-
 
 func _on_DespawnTimer_timeout():
 	queue_free()
