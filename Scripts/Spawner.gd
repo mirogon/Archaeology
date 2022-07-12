ab778f8b5a7bca8df0b26a514a38e870d9b3ab96
Marcel Römer <m1smr@hotmail.com>
2022-07-12 22:04:20 +0200

    Enemy Spawner

---
+++ Scripts/Spawner.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+export var scene_to_spawn: PackedScene
+export var min_interval_ms: int
+export var max_interval_ms: int
+export var max_spawns = 99
+
+var current_interval = 0
+var time_interval = 0
+var spawns = 0
+
+func _ready():
+	current_interval = random_interval(min_interval_ms, max_interval_ms)
+
+func random_interval(min_interval, max_interval):
+	var r = rand_range(min_interval, max_interval)
+	var interval_sec: float = r/1000
+	return interval_sec
+
+func _process(delta):
+	if spawns >= max_spawns:
+		return
+	time_interval += delta
+	if time_interval >= current_interval:
+		time_interval = 0
+		current_interval = random_interval(min_interval_ms, max_interval_ms)
+		spawn()
+
+func spawn():
+	var instance = scene_to_spawn.instance()
+	instance.position = position
+	get_tree().get_root().add_child(instance)
+	spawns += 1
