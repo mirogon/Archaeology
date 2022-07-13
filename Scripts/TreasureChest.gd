a3a58cbd83a4ceb46e9a670f34994dbd4bac065c
Marcel Römer <m1smr@hotmail.com>
2022-07-13 16:00:22 +0200

    Treasure Chest open delay, UiLayoutTest

--- Scripts/TreasureChest.gd
+++ Scripts/TreasureChest.gd
@@ -8,16 +8,19 @@ export var max_spawns = 6
 
 var open = false
 
+var time = 0
+var time_opened = 0
+
 func _ready():
-	pass # Replace with function body.
+	$OpenTimer.connect("timeout", self, "on_opentimer_timeout")
 
 func open_chest():
 	if open:
 		return
 	$AnimatedSprite.play("Chest_Open")
 	open = true
-	spawn_items()
-	
+	$OpenTimer.start()
+
 func spawn_items():
 	var num_items = (randi() % max_spawns) + 1
 	for i in range(num_items):
@@ -26,5 +29,5 @@ func spawn_items():
 		coin.position = position
 		get_parent().get_node("Pickups").add_child(coin)
 	
-#func _process(delta):
-#	pass
+func on_opentimer_timeout():
+	spawn_items()
