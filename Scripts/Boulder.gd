a7dd3901cc76bb21b2ecffb89c1f6bf1a894f3eb
Marcel Römer <m1smr@hotmail.com>
2022-07-13 16:21:20 +0200

    Boulder is coming for you!

--- Scripts/Boulder.gd
+++ Scripts/Boulder.gd
@@ -1,17 +1,21 @@
-extends KinematicBody2D
+extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var hit_damage = 100
+export var move_speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("Boulder_Move")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	position += Vector2(move_speed * delta, 0)
+
+func _on_Boulder_area_entered(area):
+	if area.get_parent().is_in_group("Player"):
+		var player = area.get_parent() as Player
+		player.get_node("HealthSystem").take_damage(hit_damage)
+
+	if area is Soldier:
+		area.get_node("HealthSystem").take_damage(hit_damage)
--- Scripts/Ui.gd
+++ Scripts/Ui.gd
@@ -9,13 +9,18 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_parent().get_node("Soldier/HealthSystem").connect("died", self, "on_soldier_died")
-
+	get_parent().get_node("Player/HealthSystem").connect("died", self, "on_player_died")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func on_soldier_died():
+	$Dead.visible = true
+	$DeadLabel.visible = true
+	get_tree().paused = true
+
+func on_player_died():
 	$Dead.visible = true
 	$DeadLabel.visible = true
 	get_tree().paused = true
