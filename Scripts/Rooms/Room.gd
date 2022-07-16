4bd0c56154b4347db1b9785ab9f82233de25c1e2
Marcel Römer <m1smr@hotmail.com>
2022-07-16 21:23:24 +0200

    Now able to walk from Room1 to Room7

--- Scripts/Rooms/Room.gd
+++ Scripts/Rooms/Room.gd
@@ -5,7 +5,9 @@ signal entered_room_traverser
 var traversed: bool = false
 
 func _ready():
-	get_node("RoomTraverser").connect("area_entered", self, "on_room_traverser_entered")
+	var traverser = get_node("RoomTraverser")
+	if traverser:
+		traverser.connect("area_entered", self, "on_room_traverser_entered")
 	
 func on_room_traverser_entered(area):
 	if area.is_in_group("Player") && traversed == false:
